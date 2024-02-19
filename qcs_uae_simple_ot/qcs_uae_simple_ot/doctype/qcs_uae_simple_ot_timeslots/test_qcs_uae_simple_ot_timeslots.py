f48d9748cd839d63b144ee78aeb27e4ce713ce3e
hiraniya <hiraniyas@gmail.com>
2024-02-19 12:28:28 +0000

    added doctypes

---
+++ qcs_uae_simple_ot/qcs_uae_simple_ot/doctype/qcs_uae_simple_ot_timeslots/test_qcs_uae_simple_ot_timeslots.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, QCS and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestQCSUAESimpleOTTimeslots(FrappeTestCase):
+	pass
