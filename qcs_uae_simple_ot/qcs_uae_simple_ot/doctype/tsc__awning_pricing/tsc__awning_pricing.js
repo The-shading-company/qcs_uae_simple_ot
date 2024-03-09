9690b9938d2915edf4a5ff68e32b5612a0388057
hiraniya <hiraniyas@gmail.com>
2024-03-09 11:22:18 +0400

    tsc - normal doctype

--- qcs_uae_simple_ot/qcs_uae_simple_ot/doctype/tsc__awning_pricing/tsc__awning_pricing.js
+++ qcs_uae_simple_ot/qcs_uae_simple_ot/doctype/tsc__awning_pricing/tsc__awning_pricing.js
@@ -1,10 +1,10 @@
 // Copyright (c) 2024, QCS and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('TSC Awning Pricing', {
+frappe.ui.form.on('TSC  Awning Pricing', {
 	awning_item: function(frm) {
 		frappe.call({
-            method: 'qcs_as_so_po.qcs_so_and_po.doctype.tsc_awning_pricing.tsc_awning_pricing.awning_item_cost',
+            method: 'qcs_uae_simple_ot.qcs_uae_simple_ot.doctype.tsc__awning_pricing.tsc__awning_pricing.awning_item_cost',
             args: {
                 "awning_item": cur_frm.doc.awning_item,
             },
@@ -15,7 +15,7 @@ frappe.ui.form.on('TSC Awning Pricing', {
 	},
 	canopy_item: function(frm) {
 		frappe.call({
-            method: 'qcs_as_so_po.qcs_so_and_po.doctype.tsc_awning_pricing.tsc_awning_pricing.canopy_item_cost',
+            method: 'qcs_uae_simple_ot.qcs_uae_simple_ot.doctype.tsc__awning_pricing.tsc__awning_pricing.canopy_item_cost',
             args: {
                 "canopy_item": cur_frm.doc.canopy_item,
             },
