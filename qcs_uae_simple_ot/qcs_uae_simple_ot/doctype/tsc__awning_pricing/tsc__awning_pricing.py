7738c20ec70028d831c7f00499efd5fe18a63c7b
Vivek <vivek@digambernath.org>
2024-03-11 14:19:24 +0400

    added cost

--- qcs_uae_simple_ot/qcs_uae_simple_ot/doctype/tsc__awning_pricing/tsc__awning_pricing.py
+++ qcs_uae_simple_ot/qcs_uae_simple_ot/doctype/tsc__awning_pricing/tsc__awning_pricing.py
@@ -20,7 +20,7 @@ class TSCAwningPricing(Document):
 
 		self.total_cost = self.awning_bom_cost + self.canopy_bom_cost
 		self.total_selling = (self.awning_bom_cost + self.canopy_bom_cost) * self.total_selling_multiplier
-		self.product_description = "Awning Item"+" - "+doc.description+"\n"+"Canopy Item"+" - "+doc1.description + " + 400AED Installation + 5%VAT"
+		self.product_description = "Awning Item"+" - "+doc.description+"\n"+"Canopy Item"+" - "+doc1.description + " : " + str(round(self.total_selling)) + " + 400AED Installation + 5%VAT"
 
 
 	 
