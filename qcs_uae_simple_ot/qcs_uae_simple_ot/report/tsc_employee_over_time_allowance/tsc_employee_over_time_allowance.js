f1ab3d82ba25cd4008d07d2459be1e9193b1506c
hiraniya <hiraniyas@gmail.com>
2024-06-24 11:27:03 +0000

    employee over time report

---
+++ qcs_uae_simple_ot/qcs_uae_simple_ot/report/tsc_employee_over_time_allowance/tsc_employee_over_time_allowance.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2024, QCS and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["TSC Employee Over Time Allowance"] = {
+	"filters": [
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"reqd": 1,
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname":"employee",
+			"label": __("Employee"),
+			"fieldtype": "Link",
+			"options": "Employee",
+		},
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},
+
+	]
+};
